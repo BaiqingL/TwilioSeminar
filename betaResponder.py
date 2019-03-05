9ca10fa068a5a20e2125d530d501dd07da03e695
AndyL <baiqinglyu@gmail.com>
2019-03-05 17:56:00 -0500

    Added AI

--- betaResponder.py
+++ betaResponder.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, redirect
+import feedparser
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from random import randint
-import feedparser
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 app = Flask(__name__)
 
@@ -17,6 +19,63 @@ def getHeadlines(rss_url):
         headlines.append(newsitem['title'])
     return headlines
 
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    actualmsg = request.values.get('Body', None)
+    p = request.values.get('From', None)
+    if p not in persons:
+        persons.append(p)
+        prev.append([])
+
+    i = persons.index(p)
+
+    body = actualmsg.lower()
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+    # Determine the right reply for this message
+    try:
+        if body == 'hello' or body == 'hi':
+            rsp(i, resp, "Hi! What is your name?")
+        elif prev[i][len(prev[i]) - 1] == "Hi! What is your name?":
+            rsp(i, resp, "Nice to meet you " + actualmsg + ". I am a chatbot. How are you doing?")
+        elif "How are you doing?" in prev[i][len(prev[i]) - 1]:
+            if "good" in body or "great" in body:
+                rsp(i, resp, "I'm glad to hear that!")
+            elif "bad" in body or "sad" in body:
+                rsp(i, resp, "I'm sad to hear that!")
+            else:
+                rsp(i, resp, "That's ok, I guess!")
+        elif body == "what is the news?":
+            rsp(i, resp, allheadlines[0])
+            rsp(i, resp, "Do you want me to keep going?")
+        elif "Do you want me to keep going?" in prev[i][len(prev[i]) - 1]:
+            if "yes" in body or "y" in body or "sure" in body:
+                rsp(i, resp, allheadlines[1])
+            else:
+                rsp(i, resp, "Ok, today was boring I guess")
+        elif body == "tell me a joke":
+            j = randint(0, len(j1) - 1)
+            rsp(i, resp, j1[j])
+            rsp(i, resp, j2[j])
+        elif body == 'bye':
+            rsp(i, resp, "Goodbye! I will forget what you just said.")
+            del prev[i][:]
+        else:
+            # Just in case the bot doesn't understand
+            rsp(i, resp, str(chatbot.get_response(actualmsg)))
+    except:
+        rsp(i, resp, str(chatbot.get_response(actualmsg)))
+    print(prev[i])
+    
+    return str(resp)
+
+
+def rsp(i, resp, ins):
+    prev[i].append(ins)
+    resp.message(ins)
 
 allheadlines = []
 
@@ -33,66 +92,10 @@ j2 = ["Because they lactose", "how much should I sell it for when I adjust for i
 persons = []
 prev = []
 
-
-@app.route("/sms", methods=['GET', 'POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    actualmsg = request.values.get('Body', None)
-    p = request.values.get('From', None)
-    if p not in persons:
-        persons.append(p)
-        prev.append([])
-
-    i = persons.index(p)
-
-    body = actualmsg.lower()
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Determine the right reply for this message
-
-    if body == 'hello' or body == 'hi':
-        rsp(i, resp, "Hi! What is your name?")
-    elif prev[i][len(prev[i]) - 1] == "Hi! What is your name?":
-        rsp(i, resp, "Nice to meet you " + actualmsg + ". I am a chatbot. How are you doing?")
-    elif "How are you doing?" in prev[i][len(prev[i]) - 1]:
-        if "good" in body or "great" in body:
-            rsp(i, resp, "I'm glad to hear that!")
-        elif "bad" in body or "sad" in body:
-            rsp(i, resp, "I'm sad to hear that!")
-        else:
-            rsp(i, resp, "That's ok, I guess!")
-    elif body == "what is the news?":
-        rsp(i, resp, allheadlines[0])
-        rsp(i, resp, "Do you want me to keep going?")
-    elif "Do you want me to keep going?" in prev[i][len(prev[i]) - 1]:
-        if "yes" in body or "y" in body or "sure" in body:
-            rsp(i, resp, allheadlines[1])
-        else:
-            rsp(i, resp, "Ok, today was boring I guess")
-
-    elif body == "tell me a joke":
-        j = randint(0, len(j1) - 1)
-        rsp(i, resp, j1[j])
-        rsp(i, resp, j2[j])
-    elif body == 'bye':
-        rsp(i, resp, "Goodbye! I will forget what you just said.")
-        del prev[i][:]
-
-    else:
-        # Just in case the bot doesn't understand
-        rsp(i, resp, "I didn't understand '" + actualmsg + "' my programmer hasn't taught me that yet")
-
-    print(prev[i])
-
-    return str(resp)
-
-
-def rsp(i, resp, ins):
-    prev[i].append(ins)
-    resp.message(ins)
-
+# Let's train the bot using the server logs
+chatbot = ChatBot('AI')
+trainer = ListTrainer(chatbot)
+trainer.train(str(open("logs.txt","r").read()).split("\n"))
 
 if __name__ == "__main__":
     app.run(debug=True)
